5e3e92bb41d57422ab9762f9338a717b3626244a
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2023-10-15 22:40:27 +0300

    feat:account image

--- flaskblog/form.py
+++ flaskblog/form.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from flaskblog.models import Users,Posts
-
+from flask_login import current_user
+from flask_wtf.file import FileField,FileAllowed
 # Start of register Form
 
 
@@ -39,3 +40,33 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     sumbit = SubmitField('Login')
 # end of login Form
+
+#account update form
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    Email = StringField('Email', validators=[
+        DataRequired(), Length(min=2, max=20), Email()])
+    picture =FileField('upload picture',validators=[FileAllowed(['jpg','png'])])
+    sumbit = SubmitField('update')
+    def validate_username(self, username):
+        if username.data != current_user.Username:
+            user = Users.query.filter_by(Username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+        
+    def validate_Email(self,    Email):
+        if Email.data != current_user.Email:
+            user = Users.query.filter_by(Email=Email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+
+class Postform(FlaskForm):    
+    title :str= StringField('Title', validators=[
+                           DataRequired(), Length(min=2, max=200)])   
+
+    content=TextAreaField('content',validators=[DataRequired()])
+    sumbit = SubmitField('create new post')
+
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,10 +1,12 @@
 import time
+import secrets
+from PIL import Image
 from flaskblog.models import Users, Posts
 from flask import render_template, url_for, flash, redirect,request
-from flaskblog.form import RegistriationForm, LoginForm
+from flaskblog.form import RegistriationForm, LoginForm,UpdateAccountForm
 from flaskblog import app,db,bcrypt,login_manager
 from flask_login import login_user,current_user,logout_user,login_required
-
+import os
 # craeting database  SQLAlCHEMY
 posts = [
     {
@@ -84,10 +86,46 @@ def logout():
    logout_user()
    return redirect(url_for('home'))
 
+def save_picture(form_picture):
+   random_hex=secrets.token_hex(8)
+   _,f_ext=os.path.splitext(form_picture.filename)
+   picture_fn=random_hex+f_ext
+   picture_path=os.path.join(app.root_path,'static/profile_pic',picture_fn)
+   output=[125,125]
+   i=Image.open(form_picture)
+   i.thumbnail(output)
 
-@app.route("/Account")
+   i.save(picture_path)
+   return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
-def Account():
+def account():
+     formm=UpdateAccountForm()
+     if formm.validate_on_submit():
+        if formm.picture.data:
+           picture_file=save_picture(formm.picture.data)
+           current_user.Image_file=picture_file
+        current_user.Username=formm.username.data
+        current_user.Email=formm.Email.data
+        db.session.commit()
+        flash('success nice ', 'success')
+        return redirect(url_for('account'))
+     elif request.method=='GET':
+       formm.username.data=current_user.Username
+       formm.Email.data=current_user.Email
+       image_file=url_for('static',filename='profile_pic/'+current_user.Image_file)
+     image_file=url_for('static',filename='profile_pic/'+current_user.Image_file)  
      if current_user.is_authenticated:
-      return render_template('Account.html',title='Account',user=current_user)
+        return render_template('Account.html',title='Account',current_user=current_user,img=image_file,form=formm)
   
+
+
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+   return render_template('create_post.html',title='create post')
+   
