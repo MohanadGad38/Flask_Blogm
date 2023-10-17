e7c56e152541c07cb1eab11b656fe9ad6176adc5
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2023-10-17 17:17:19 +0300

    FIX:os environ

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 import os
-
+from dotenv import load_dotenv
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f6ea78cf69d66fbda36bed5de1694377'
 # database config
@@ -21,10 +21,10 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 #app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_M')
 #app.config['MAIL_PASSWORD'] = os.environ.get('PASS_M')
-print(os.environ.get('EMAI_M'))
-print(os.environ.get('PASS_M'))
-print(os.environ)
+load_dotenv('/Users/mohanadgad/bash_profile.env')
 
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_M')
+app.config['MAIL_PASSWORD'] =os.environ.get('PASS_M')
 mail = Mail(app)
 
 with app.app_context():
